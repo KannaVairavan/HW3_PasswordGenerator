1764ea296a8039f75190d3981f262a975e4c408b
Kannathal Vairavan <kannamanickam@gmail.com>
2021-01-10 21:45:11 -0600

    Added generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,19 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+const randomObj = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  spchar: randomSpecialChar,
+}
+console.log (Math.floor(Math.random()*26)+97);
 // Write password to the #password input
 function writePassword() {
     
     var pwLength
-    var pwLower
-    var pwUpper
-    var pwNumber
-    var pwSpecialChar
-
+    
+    
     pwLength = prompt("Enter password length? Choose between 8 to 128.");
     if(!pwLength){
       pwLength = prompt("You must enter password length between 8 to 128.");
@@ -24,12 +28,12 @@ function writePassword() {
         // Validate user Input
       pwLength = prompt("You must enter password length between 8 to 128.");
     }
-      pwLower = confirm("Will this contain lower case letters?");
-      pwUpper = confirm("Will this contain upper case letters?");
-      pwNumber = confirm("Will this contain number?");
-      pwSpecialChar =confirm("will this contain special characters?");
+      var pwLower = confirm("Will this contain lower case letters?");
+      var pwUpper = confirm("Will this contain upper case letters?");
+      var pwNumber = confirm("Will this contain number?");
+      var pwSpecialChar =confirm("will this contain special characters?");
     
-     console.log(pwLower, pwUpper, pwNumber, pwSpecialChar, pwLength) 
+     console.log(pwLower, pwUpper, pwNumber, pwSpecialChar, pwLength) ;
 
     // Validtate all negative option
     if(!pwLower && !pwUpper && !pwNumber && !pwSpecialChar){
@@ -43,29 +47,47 @@ function writePassword() {
  
 }
 // generate password function
-// generatePassword(lower, upper, number, spchar, length){
-
-// }
-  // Confirm function
-function pwConfirm(){
-   
+function generatePassword(lower, upper, number, spchar, length){
+  // init variable
+  // check password confirm types and filter out false type
+  // loop through the password length and call random function
+  //assign to password variable
+  var generatedPassword='';
+  const typeCount=lower + upper + number + spchar;
+  const typearray=[{lower}, {upper}, {number}, {spchar}].filter
+  (
+    item => Object.values(item)[0]
+  );
+  console.log ('typearray: ', typearray);
+  if (typeCount === 0 ){
+    return '';
+  }
+  for (var i = 0; i < length; i += typeCount ){
+    typearray.forEach(type =>{
+      const pwobj = Object.keys(type)[0]
+      generatedPassword += randomObj[pwobj]();
+    });
+  }
+  console.log(generatedPassword);
+  return generatedPassword;
 }
+  
 
 function randomLower(){
-  return String.fromCharCode(math.floor( Math.random()*26)+97);
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
 }
 // converts letters to uppercase 
 function randomUpper(){
-  return String.fromCharCode(math.floor( Math.random()*26)+65);
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
 }
 
 function randomNumber(){
-  return String.fromCharCode(math.floor( Math.random()*10)+48);
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
 
 function randomSpecialChar(){
   const specialChar = "!@#$*%+-&~|/"
-  return specialChar[math.floor( Math.random()*specialChar.length)];
+  return specialChar[Math.floor(Math.random()*specialChar.length)];
 }
 
 
